660e5046d3c41bb0af947c6d9d64933f483601e6
JWP <98960141+qkrwldnjs@users.noreply.github.com>
2022-11-06 03:25:15 +0900

    Fixed time format

--- src/components/Charts/UpdateTime.jsx
+++ src/components/Charts/UpdateTime.jsx
@@ -20,8 +20,8 @@ const UpdateTime = (props) => {
     const date = new Date(update * 1000);
     return (
         <div className="updated fadein">
-            최종 업데이트 | {date.getFullYear()}.{date.getMonth() + 1}.
-            {date.getDate()} {date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}:
+            최종 업데이트 | {date.getFullYear()}.{date.getMonth() + 1 < 10 ? "0" + date.getMonth() + 1 : date.getMonth() + 1}.
+            {date.getDate() < 10 ? "0" + date.getDate() : date.getDate()} {date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}:
             {date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}
         </div>
     )
